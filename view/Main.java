74f9a95e9722cfb447c0938b4855df9c9d60e9b1
R-Torrent <rta@alumnes.upm.es>
2021-07-23 23:14:21 +0200

    Milestone 1: Menú principal; paquete 'view' - 2ª versión (Factory)

--- view/Main.java
+++ view/Main.java
@@ -1,4 +1,6 @@
-/** Back-end Java, IT Academy */
+/** Back-end Java, IT Academy
+*** M11 - Milestone 1
+*** Miguel Blasco & Roger Torrent */
 
 package view;
 
@@ -7,35 +9,35 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
 
-import domain.*;
 import application.Controller;
+import domain.Floristeria;
 
 public class Main extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
 	private final JPanel panell;
-	private final JButton crearFloristeria, mostrarStock, afegirArbre, afegirFlor, afegirDecoracio, sortir;
-	private final JComboBox<String> llistaFloristeries;
+	private final JButton crearFloristeria, mostrarStock, sortir;
+	final JComboBox<String> llistaFloristeries;
 	
-	private Controller m11;
+	private final ButtonsProducteFactory[] factoriesBotons = {
+		new ButtonsArbreFactory(),
+		new ButtonsFlorFactory(),
+		new ButtonsDecoracioFactory(),
+		// Nuevas líneas de productos se añaden aquí
+	};
+	private ButtonProducte[] botonsAfegir = new ButtonProducte[3];
+	
+	Controller m11;
 	
 	public static void main(String[] args) {
 		new Main().start();
@@ -62,43 +64,11 @@ public class Main extends JFrame {
 		box1.add(mostrarStock);
 		panell.add(box1);
 		
-		afegirArbre = new JButton("AfegirArbre");
-		afegirFlor = new JButton("AfegirFlor");
-		afegirDecoracio = new JButton("AfegirDecoració");
-		afegirArbre.setEnabled(false);
-		afegirFlor.setEnabled(false);
-		afegirDecoracio.setEnabled(false);
-		Box box2 = Box.createHorizontalBox();
-		box2.add(afegirArbre);
-		box2.add(Box.createHorizontalStrut(5));
-		box2.add(afegirFlor);
-		box2.add(Box.createHorizontalStrut(5));
-		box2.add(afegirDecoracio);
-		panell.add(Box.createVerticalStrut(10));
-		panell.add(box2);
-		
-		sortir = new JButton("Sortir");
-		sortir.setAlignmentX(JFrame.CENTER_ALIGNMENT);
-		panell.add(Box.createVerticalStrut(10));
-		panell.add(sortir);
-		
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
-	// Método principal; instancia el controlador y activa todos los 'listeners' de los botones
-	private void start() {
-		m11 = new Controller();
-		
 		// Floristería nueva
 		crearFloristeria.addActionListener(new ActionListener() {
 			
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent ev) {
 				String nom = JOptionPane.showInputDialog(panell, "Nom de la floristeria:",
 						"Nova floristeria", JOptionPane.QUESTION_MESSAGE);
 				
@@ -106,9 +76,8 @@ public class Main extends JFrame {
 					llistaFloristeries.addItem(nom);
 					llistaFloristeries.setSelectedItem(nom);
 					mostrarStock.setEnabled(true);
-					afegirArbre.setEnabled(true);
-					afegirFlor.setEnabled(true);
-					afegirDecoracio.setEnabled(true);
+					for(int i = 0; i < botonsAfegir.length; i++)
+						botonsAfegir[i].setEnabled(true);
 				} } catch (Exception ex) { error(ex); }
 			}
 			
@@ -136,23 +105,29 @@ public class Main extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String stock = m11.stock(llistaFloristeries.getSelectedIndex());
-				JOptionPane.showMessageDialog(panell, stock, "Stock \'" + llistaFloristeries.getSelectedItem() + "\'",
-						JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(panell, m11.stock(llistaFloristeries.getSelectedIndex()),
+						"Stock \'" + llistaFloristeries.getSelectedItem() + "\'",
+								JOptionPane.INFORMATION_MESSAGE);
 			}
 			
 		});
 		
-		// Añadir un árbol al inventario
-		afegirArbre.addActionListener(new ActionListenerProducte("Arbre nou"));
-		
-		// Añadir una flor al inventario
-		afegirFlor.addActionListener(new ActionListenerProducte("Flor nova"));
-		
-		// Añadir una decoración al inventario
-		afegirDecoracio.addActionListener(new ActionListenerProducte("Decoració nova"));
+		// Botones 'AfegirXXX', de las factorías pertinentes
+		Box box2 = Box.createHorizontalBox();
+		for(int i = 0; i < factoriesBotons.length; i++) {
+			botonsAfegir[i] = factoriesBotons[i].afegirButton();
+			box2.add(botonsAfegir[i]);
+			if(i < factoriesBotons.length-1)
+				box2.add(Box.createHorizontalStrut(5));
+		}
+		panell.add(Box.createVerticalStrut(10));
+		panell.add(box2);
 		
 		// Salida del programa
+		sortir = new JButton("Sortir");
+		sortir.setAlignmentX(JFrame.CENTER_ALIGNMENT);
+		panell.add(Box.createVerticalStrut(10));
+		panell.add(sortir);
 		sortir.addActionListener(new ActionListener() {
 			
 			@Override
@@ -163,6 +138,17 @@ public class Main extends JFrame {
 			}
 			
 		});
+		
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	// Método principal; instancia el controlador
+	private void start() {
+		m11 = new Controller();
 	}
 	
 	// Mensaje de error
@@ -170,85 +156,38 @@ public class Main extends JFrame {
 		JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
 	}
 	
-	// Clase común a los 3 botones 'afegirXXX'
-	private class ActionListenerProducte implements ActionListener {
+	// Generación de los botones particulares a cada línea
+	// de productos mediante el patrón "Abstract Factory".
+	interface ButtonsProducteFactory {
 		
-		private final String title;
-		private JPanel panellInput;
-		private JTextField nom, preu, dada;
-		private List<JRadioButton> materials;
-		private ButtonGroup grupMaterials;
-		
-		private ActionListenerProducte(String title) {
-			this.title = title;
-		}
+		ButtonAfegirProducte afegirButton();
+//		ButtonRetirarProducte retirarButton();  <-- NUEVAS FUNCIONALIDAD PARA MILESTONE 2
+//		ButtonRetirarProducte stockButton();    <-- NUEVAS FUNCIONALIDAD PARA MILESTONE 2
+	}
+	
+	class ButtonsArbreFactory implements ButtonsProducteFactory {
 		
 		@Override
-		public void actionPerformed(ActionEvent ev) {
-			panellInput = new JPanel();
-			panellInput.setLayout(new BoxLayout(panellInput, BoxLayout.Y_AXIS));
-			
-			panellInput.add(new JLabel("Nom del producte:"));
-			nom = new JTextField();
-			panellInput.add(nom);
-			panellInput.add(Box.createVerticalStrut(10));
-			panellInput.add(new JLabel("Preu del producte:"));
-			preu = new JTextField();
-			panellInput.add(preu);
-			panellInput.add(Box.createVerticalStrut(10));
-			switch(ev.getActionCommand()) {
-			case("AfegirArbre"):
-				panellInput.add(new JLabel("Alçada del arbre (cm):"));
-				dada = new JTextField();
-				panellInput.add(dada);
-				break;
-			case("AfegirFlor"):
-				panellInput.add(new JLabel("Color de la flor:"));
-				dada = new JTextField();
-				panellInput.add(dada);
-				break;
-			case("AfegirDecoració"):
-				panellInput.add(new JLabel("Material de la decoració:"));
-				materials = new ArrayList<>();
-				grupMaterials = new ButtonGroup();
-				Stream.of(Decoracio.Material.values()).forEach(t -> materials.add(new JRadioButton(t.name())));
-				materials.forEach(m -> {
-					grupMaterials.add(m);
-					m.setActionCommand(m.getText());
-					panellInput.add(m);
-				});
-				break;
-			default: return;
-			}
-			
-			if(JOptionPane.showConfirmDialog(panell, panellInput, title,
-					JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) try {
-				String error1 = "Format numèric erroni o absent.\n"
-						+ "NOTA: Les mesures en cm són nombres enters, i el separador decimal dins el preu és el punt.";
-				String error2 = "Tipus de material sense especificar.";
-				try {
-					Float fpreu = Float.parseFloat(preu.getText());
-					switch(ev.getActionCommand()) {
-					case("AfegirArbre"):
-						Integer alcada = Integer.parseInt(dada.getText());
-						m11.afegirProducte(llistaFloristeries.getSelectedIndex(),
-								new Arbre(nom.getText(), alcada, fpreu));
-						break;
-					case("AfegirFlor"):
-						m11.afegirProducte(llistaFloristeries.getSelectedIndex(),
-								new Flor(nom.getText(), dada.getText(), fpreu));
-						break;
-					case("AfegirDecoració"):
-						if(grupMaterials.getSelection() == null) throw new Exception(error2);
-						m11.afegirProducte(llistaFloristeries.getSelectedIndex(),
-								new Decoracio(nom.getText(),
-										Decoracio.Material.valueOf(grupMaterials.getSelection().getActionCommand()),
-											 fpreu));
-						break;
-					default:
-					}
-				} catch (NullPointerException | NumberFormatException ex) { throw new Exception(error1); }
-			} catch (Exception ex) { error(ex); }
+		public ButtonAfegirProducte afegirButton() {
+			return new ButtonAfegirArbre("AfegirArbre", "Arbre nou", Main.this); 
+		}
+		
+	}
+	
+	class ButtonsFlorFactory implements ButtonsProducteFactory {
+		
+		@Override
+		public ButtonAfegirProducte afegirButton() {
+			return new ButtonAfegirFlor("AfegirFlor", "Flor nova", Main.this);
+		}
+		
+	}
+	
+	class ButtonsDecoracioFactory implements ButtonsProducteFactory {
+		
+		@Override
+		public ButtonAfegirProducte afegirButton() {
+			return new ButtonAfegirDecoracio("AfegirDecoració", "Decoració nova", Main.this);
 		}
 		
 	}
